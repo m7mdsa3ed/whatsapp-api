46651f3255b7625bb19357a2087faa8c04c81678
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2022-12-05 00:28:01 +0200

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 require('dotenv').config()
 require('./libs/mongodb').connect()
+
+const corsConfigs = {
+  origin: '*'
+}
 
 const routes = require('./routes')
 
@@ -13,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-
+app.use(cors(corsConfigs));
 app.use('/', routes)
 
 module.exports = app;
